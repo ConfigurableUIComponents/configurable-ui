d49ba0c496b032a71c6fbd7435e6410fb28f3610
mattanb <bitner.mattan@gmail.com>
2018-04-17 18:39:30 +0300

    add source-map-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 module.exports = {
+  devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre"
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
